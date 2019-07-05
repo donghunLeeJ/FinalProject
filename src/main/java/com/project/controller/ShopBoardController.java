2c3c2ca94355ef344455e5ae64f43465e47282a9
Jack <dlgodud8997@naver.com>
2019-07-05 16:05:40 +0900

    donghun

---
+++ src/main/java/com/project/controller/ShopBoardController.java
@@ -0,0 +1,11 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Controller
+@RequestMapping("/shopboard")
+public class ShopBoardController {
+	
+}
